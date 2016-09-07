caaf5ebd42fc64923327c7c167f086ff0c181e37
Mike Bostock <mbostock@gmail.com>
2016-09-07 10:30:55 -0700

    Yield promise on open.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import slice from "slice-source";
 import fetchPath from "./fetch";
 import requestPath from "./request";
 
 export default function path(path) {
-  return slice((typeof fetch === "function" ? fetchPath : requestPath)(path));
+  return (typeof fetch === "function" ? fetchPath : requestPath)(path);
 }
--- request.js
+++ request.js
@@ -1,48 +1,13 @@
+import array from "array-source";
+
 export default function(url) {
-  return new RequestSource(url);
-}
-
-function RequestSource(url) {
-  var that = this,
-      request = this._request = new XMLHttpRequest,
-      readResolve,
-      readReject;
-
-  this._read = new Promise(function(resolve, reject) {
-    readResolve = resolve;
-    readReject = reject;
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest;
+    request.responseType = "arraybuffer";
+    request.onload = function() { resolve(array(request.response)); };
+    request.onerror = reject;
+    request.ontimeout = reject;
+    request.open("GET", url, true);
+    request.send();
   });
-
-  request.responseType = "arraybuffer";
-  request.onload = loaded;
-  request.onerror = errored;
-  request.ontimeout = errored;
-  request.open("GET", url, true);
-  request.send();
-
-  function loaded() {
-    that._read = Promise.resolve({done: true, value: undefined});
-    readResolve({done: false, value: new Uint8Array(request.response)});
-  }
-
-  function errored(error) {
-    that._read = Promise.reject(error);
-    readReject(error);
-  }
 }
-
-RequestSource.prototype.read = function() {
-  return this._read;
-};
-
-RequestSource.prototype.cancel = function() {
-  var request = this._request;
-  switch (request.readyState) {
-    case 0: case 4: return Promise.resolve();
-    default: return new Promise(function(resolve, reject) {
-      request.onerror = function(error) { reject(error); };
-      request.onabort = function() { resolve(); };
-      request.abort();
-    });
-  }
-};
